1b47249c7f509d1a6620fac280c867dde397f9ca
wenhao1220 <whlee1220@gmail.com>
2024-03-24 14:58:54 +0800

    add database

---
+++ firebase.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase/app';
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBGclJJtgLEFZCXXrkpdCFZO22Rzq85qj0",
+    authDomain: "customized-b24d0.firebaseapp.com",
+    projectId: "customized-b24d0",
+    storageBucket: "customized-b24d0.appspot.com",
+    messagingSenderId: "1036057663724",
+    appId: "1:1036057663724:web:8f9a41825b488be6e1adc9",
+    measurementId: "G-VK6CBBGVKH"
+};
+
+firebase.initializeApp(firebaseConfig)
+
+export default firebase;
